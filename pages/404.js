77c1240d9a35dd6718ce55f4c30e8e6dba587af2
Thamires Marques <109435248+thamiresvpmarques@users.noreply.github.com>
2023-06-17 10:14:53 -0300

    subindo o projeto

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Link from "../src/components/link";
+
+export default function Page404() {
+    return(
+        <div>
+          
+          <h1>Você se perdeu e caiu na página 404</h1>
+          <Link href="/">
+             Ir para home 
+          </Link>
+        
+        </div>
+    )
+}
